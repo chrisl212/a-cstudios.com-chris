9a1902306f87b5d8a365784a59530a8c874e9a4f
Christopher Loonam <christopherloonam@ChristosMacBook.home>
2015-09-21 15:01:36 -0400

    project page made dynamic

--- projects/index.php
+++ projects/index.php
@@ -1,7 +1,19 @@
 <!DOCTYPE html>
+<?php
+	$title = $_GET["project"];
+	$filePath = $title . $title . "json";
+	$fileContents = file_get_contents($filePath);
+	$rootDictionary = json_decode($fileContents, true);
+
+	$name = $rootDictionary["name"];
+	$description = $rootDictionary["desc"];
+	$itunesURL = $rootDictionary["itunes"];
+	$githubURL = $rootDictionary["github"];
+	$imageFilePath = $title . $title . "png";
+?>
 <html>
 	<head>
-		<title>404 Page Not Found</title>
+		<title><?=$name?></title>
 		<link rel="stylesheet" href="/style.css" />
 		<script src="/detectmobilebrowser.js"></script>
 	</head>
@@ -27,8 +39,11 @@
 			<hr />
 		</nav>
 		<div id="content">
-			<h2 style="text-align:center;">404 Page Not Found</h2>
-			<p style="text-align:center;">The page you requested does not exist.</p>
+			<h1 style="text-align:center;"><?=$name?></h1>
+			<img src=<?=$imageFilePath?> alt="Icon" style="width:30%; border-radius:30px;"/>
+			<p class="appDescription"><?=$description?></p>
+			<p style="text-align:center; width:30%;"><a href=<?=$itunesURL?>>View in iTunes</a></p>
+			<p style="text-align:center; width:30%;"><a href=<?=$githubURL?>>View on Github</a></p>
 		</div>
 		<hr />
 		<p style="text-align:center;">Copyright 2015 Chris Loonam</p>
